a069ebac2c9923b275bc42bfe7762a2c508b2f9c
Shicunfeng7-commits <shicunfeng7@gmail.com>
2026-06-11 00:00:28 +0800

    chore: init project structure with Spring Boot + MyBatis-Plus + MySQL

---
+++ backend/src/main/java/com/xiyiji/XiyijiApplication.java
@@ -0,0 +1,11 @@
+package com.xiyiji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiyijiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(XiyijiApplication.class, args);
+    }
+}
